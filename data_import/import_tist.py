98bb47e3f8cfbbed599b8b295c1931c09e6efd14
Henry <martinhe@ethz.ch>
2019-05-09 11:16:09 +0200

    Cleaned code (PEP8), added relative and os independent paths

--- data_import/import_tist.py
+++ data_import/import_tist.py
@@ -1,34 +1,75 @@
-import numpy as np
-import glob
-import pandas as pd
-import geopandas as gpd
-import sys
+"""
+Script to import tist data into a postgis database. Also applies trackintel
+data model
+"""
+
+import os
 from sqlalchemy import create_engine
 import psycopg2
-import time
+import pandas as pd
+import json
 
-crs_wgs = {'init' :'epsg:4326'}
-with open('login.json') as json_file:  
+
+dblogin_file = os.path.join("..", "dblogin.json")
+with open(dblogin_file) as json_file:
     login_data = json.load(json_file)
-    
+
 conn_string = "postgresql://{user}:{password}@{host}:{port}/{database}".format(**login_data)
 
 engine = create_engine(conn_string)
 conn = engine.connect()
 
-data_folder = ".\data_tist\*"
-user_folder = glob.glob(data_folder)
+path_checkins = os.path.join('data_tist', 'dataset_TIST2015_Checkins.txt')
+path_pois = os.path.join('data_tist', 'dataset_TIST2015_POIs.txt')
 
-
+# create schema
 with psycopg2.connect(conn_string) as conn2:
-    cur = conn2.cursor()    
-    
-    query = """CREATE SCHEMA if not exists tist;"""    
+    cur = conn2.cursor()
+
+    query = """CREATE SCHEMA if not exists tist;"""
     cur.execute(query)
     conn2.commit()
 
-checkins = pd.read_csv('dataset_TIST2015_Checkins.txt', sep='\t', header=None, names=['user_id,', 'venue_id', 'started_at'])
-venues = pd.read_csv('dataset_TIST2015_POIs.txt', sep='\t', header=None, names=['venue_id', 'lat', 'lon', 'category', 'country_code'])
+# load raw data
+print('Reading raw data')
+checkins = pd.read_csv(path_checkins, sep='\t', header=None,
+                       names=['user_id', 'venue_id', 'started_at', 'timezone'])
+venues = pd.read_csv(path_pois, sep='\t', header=None,
+                     names=['venue_id', 'lat', 'lon', 'category', 'country_code'])
 
+# repair unicode error
+venues.loc[venues["category"] == 'Caf', "category"] = 'Café'
+
+print('Writing to database')
 checkins.to_sql('checkins', engine, schema="tist", if_exists='append', index=False, chunksize=50000)
 venues.to_sql('venues', engine, schema="tist", if_exists='append', index=False, chunksize=50000)
+
+del checkins, venues
+
+
+with psycopg2.connect(conn_string) as conn2:
+    cur = conn2.cursor()
+
+    # create staypoints table
+    print('Create staypoint table')
+    query = """CREATE TABLE tist.staypoints as
+                    SELECT checkins.user_id, checkins.started_at,
+                            venues.lat, venues.lon, venues.category,
+                            checkins.timezone, venues.country_code
+                    FROM tist.checkins
+                    INNER JOIN tist.venues on checkins.venue_id = venues.venue_id;"""
+    cur.execute(query)
+    conn2.commit()
+
+    # create and fill geometry
+    print('Create staypoint geometries')
+    query = """select AddGeometryColumn(
+                'tist', 'staypoints', 'geom', 4326, 'Point', 2);"""
+    cur.execute(query)
+
+    query = """update tist.staypoints SET
+                geom = ST_SetSRID(ST_MakePoint(lon, lat), 4326);"""
+    cur.execute(query)
+    conn2.commit()
+
+print('Done')
