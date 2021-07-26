08c36afebbfb7edad67be8d7fab8cd2e5f55cbd8
Henry Martin <martinhe@ethz.ch>
2021-07-26 16:52:05 +0200

    FIX: update to new trackintel version and black formatting

--- 1_import/import_sbbdata.py
+++ 1_import/import_sbbdata.py
@@ -13,77 +13,78 @@ import numpy as np
 import os
 import json
 
-CRS_WGS84 = 'epsg:4326'
+CRS_WGS84 = "epsg:4326"
 #
-studies = ['gc2', 'gc1']
+studies = ["gc2"]  # , 'gc1']
 
-DBLOGIN_FILE = os.path.join('.','dblogin.json')
-DBLOGIN_FILE_SOURCE = os.path.join('.','dblogin_source.json')
+DBLOGIN_FILE = os.path.join(".", "dblogin.json")
+DBLOGIN_FILE_SOURCE = os.path.join(".", "dblogin_source.json")
 
-with open(DBLOGIN_FILE) as json_file:  
+with open(DBLOGIN_FILE) as json_file:
     LOGIN_DATA = json.load(json_file)
 
 with open(DBLOGIN_FILE_SOURCE) as json_file_source:
     LOGIN_DATA_SOURCE = json.load(json_file_source)
-        
+
 for study in studies:
     # build database login string from file
-    print('start study {}'.format(study))
-        
-    conn_string = "postgresql://{user}:{password}@{host}:{port}/{database}"\
-                    .format(**LOGIN_DATA)
-    conn_string_source = "postgresql://{user}:{password}@{host}:{port}/{database}"\
-                    .format(**LOGIN_DATA_SOURCE)
-    
+    print("start study {}".format(study))
+
+    conn_string = "postgresql://{user}:{password}@{host}:{port}/{database}".format(**LOGIN_DATA)
+    conn_string_source = "postgresql://{user}:{password}@{host}:{port}/{database}".format(**LOGIN_DATA_SOURCE)
+
     engine = create_engine(conn_string)
     conn = engine.connect()
 
     engine_source = create_engine(conn_string_source)
     conn_source = engine_source.connect()
 
-    print('download staypoints')    
-    sp = gpd.GeoDataFrame.from_postgis(sql="SELECT * FROM {}.staypoints".format(study),
-                                           con=conn_source,
-                                           crs=CRS_WGS84, 
-                                           geom_col='geometry_raw')
-    print('download triplegs')
-    tpls = gpd.GeoDataFrame.from_postgis(sql="SELECT * FROM {}.triplegs where ST_isValid(geometry)".format(
-        study),
-                                       con=conn_source,
-                                       crs=CRS_WGS84,
-                                       geom_col='geometry')
+    print("download staypoints")
+    sp = gpd.GeoDataFrame.from_postgis(
+        sql="SELECT * FROM {}.staypoints".format(study), con=conn_source, crs=CRS_WGS84, geom_col="geometry_raw"
+    )
+    print("download triplegs")
+    tpls = gpd.GeoDataFrame.from_postgis(
+        sql="SELECT * FROM {}.triplegs where ST_isValid(geometry)".format(study),
+        con=conn_source,
+        crs=CRS_WGS84,
+        geom_col="geometry",
+    )
 
     conn_source.close()
     sp = sp.drop("geometry", axis=1)
     tpls = tpls.drop("geometry_raw", axis=1)
 
-    sp = sp.rename(columns={'geometry_raw': 'geom'})
-    tpls = tpls.rename(columns={'geometry': 'geom'})
+    sp = sp.rename(columns={"geometry_raw": "geom"})
+    tpls = tpls.rename(columns={"geometry": "geom"})
 
     sp = sp.set_geometry("geom")
     tpls = tpls.set_geometry("geom")
 
-    # create important places 
+    # create important places
     sp["elevation"] = np.nan
-    sp['started_at'] = sp['started_at'].dt.tz_localize('UTC')
-    sp['finished_at'] = sp['finished_at'].dt.tz_localize('UTC')
+    sp["started_at"] = sp["started_at"].dt.tz_localize("UTC")
+    sp["finished_at"] = sp["finished_at"].dt.tz_localize("UTC")
 
-    tpls['started_at'] = tpls['started_at'].dt.tz_localize('UTC')
-    tpls['finished_at'] = tpls['finished_at'].dt.tz_localize('UTC')
+    tpls["started_at"] = tpls["started_at"].dt.tz_localize("UTC")
+    tpls["finished_at"] = tpls["finished_at"].dt.tz_localize("UTC")
 
-    print('create places')
-    sp, locs = sp.as_staypoints.generate_locations(method='dbscan', epsilon=15, num_samples=1,
-                                                       distance_metric='haversine', agg_level='user')
+    print("create places")
+    sp, locs = sp.as_staypoints.generate_locations(
+        method="dbscan", epsilon=30, num_samples=2, distance_metric="haversine", agg_level="user"
+    )
 
-    print('write staypoints to database')
+    print("write staypoints to database")
 
-    ti.io.write_staypoints_postgis(staypoints=sp, conn_string=conn_string, table_name="staypoints",
-                                   schema=study, if_exists="replace")
+    ti.io.write_staypoints_postgis(
+        staypoints=sp, conn_string=conn_string, name="staypoints", schema=study, if_exists="replace"
+    )
 
-    print('write triplegs')
-    ti.io.write_triplegs_postgis(triplegs=tpls, conn_string=conn_string, table_name="triplegs",
-                                   schema=study, if_exists="replace")
+    print("write triplegs")
+    ti.io.write_triplegs_postgis(
+        triplegs=tpls, conn_string=conn_string, name="triplegs", schema=study, if_exists="replace"
+    )
 
-    print('write locations to database')
-    locs = locs.drop('extent', axis=1)
-    ti.io.write_locations_postgis(locs, conn_string, schema=study, table_name="locations", if_exists='replace')
+    print("write locations to database")
+    locs = locs.drop("extent", axis=1)
+    ti.io.write_locations_postgis(locs, conn_string, schema=study, name="locations", if_exists="replace")
