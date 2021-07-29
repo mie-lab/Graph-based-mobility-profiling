3d604b13b51c8d618dc698db38fc71913f382077
Henry <martinhe@ethz.ch>
2021-07-29 21:28:25 +0200

    Black

--- future_trackintel/activity_graphs_utils.py
+++ future_trackintel/activity_graphs_utils.py
@@ -28,6 +28,7 @@ def haversine_dist_of_shapely_objs(obj1, obj2):
     """
     return haversine_dist(obj1.x, obj1.y, obj2.x, obj2.y)
 
+
 def nx_coordinate_layout(G):
     """
     Return networkx graph layout based on geographic coordinates.
@@ -47,20 +48,22 @@ def nx_coordinate_layout(G):
 
     return pos
 
+
 def nx_coordinate_layout_smopy(G, smap):
-    """"transforms WGS84 coordinates to pixel coordinates of a smopy map"""
-    node_center = nx.get_node_attributes(G, 'center')
-    pos = {key: (smap.to_pixels(geometry.y,geometry.x)) for key,geometry in node_center.items()}
+    """ "transforms WGS84 coordinates to pixel coordinates of a smopy map"""
+    node_center = nx.get_node_attributes(G, "center")
+    pos = {key: (smap.to_pixels(geometry.y, geometry.x)) for key, geometry in node_center.items()}
 
     return pos
 
+
 def draw_smopy_basemap(G, figsize=(8, 6), zoom=10, ax=None):
     """Draw a basemap with the extent given by graph G"""
-    
+
     pos_wgs = nx_coordinate_layout(G)
-    lon =  [ coords[0] for coords in pos_wgs.values() ]
-    lat =  [ coords[1] for coords in pos_wgs.values() ]
-    
+    lon = [coords[0] for coords in pos_wgs.values()]
+    lat = [coords[1] for coords in pos_wgs.values()]
+
     lon_min = min(lon)
     lon_max = max(lon)
     lat_min = min(lat)
@@ -73,16 +76,17 @@ def draw_smopy_basemap(G, figsize=(8, 6), zoom=10, ax=None):
 
             break
         except IncompleteRead as e:
-            
+
             attempts += 1
             if attempts == 3:
-                print(G.graph['user_id'], e)
+                print(G.graph["user_id"], e)
                 smap = smopy.Map(lat_min, lon_min, lat_max, lon_max)
-            
+
     # map = smopy.Map((min_y, max_y-min_y, min_x, max_x-min_x), z=5)
     ax = smap.show_mpl(figsize=figsize, ax=ax)
-    
+
     return ax, smap
+
 
 #
 # def create_activity_graphs(staypoints, places, A_dict, temporal_bin_size=None):
