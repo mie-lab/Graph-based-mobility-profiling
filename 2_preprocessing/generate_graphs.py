f982cdd41bea80412951a4e9b95234c85f90fd90
Henry Martin <martinhe@ethz.ch>
2021-07-29 14:47:00 +0200

    ENH: user filtering and trip based graphs - merge staypoints (horizontal) is moved to import scrips - graphs are now generated based on trips - users with low tracking coverage are now filtered

--- 2_preprocessing/generate_graphs.py
+++ 2_preprocessing/generate_graphs.py
@@ -62,64 +62,29 @@ def get_triplegs(study, engine):
     return tpls
 
 
-def horizontal_merge_staypoints(sp):
-    """merge staypoints that are consecutive at the same place"""
-    # merge consecutive staypoints
+def get_trips(study, engine):
+    trips = pd.read_sql(sql="select * from {}.trips {}".format(study, limit), con=engine, index_col="id")
+    trips["started_at"] = pd.to_datetime(trips["started_at"], utc=True)
+    trips["finished_at"] = pd.to_datetime(trips["finished_at"], utc=True)
 
-    sp_merge = sp.copy()
-    assert sp_merge.index.name == "id", "expected index name to be 'id'"
-
-    sp_merge = sp_merge.reset_index()
-    sp_merge.sort_values(inplace=True, by=["user_id", "started_at"])
-    sp_merge[["next_started_at", "next_location_id"]] = sp_merge[["started_at", "location_id",]].shift(-1)
-    cond = pd.Series(data=False, index=sp_merge.index)
-    cond_old = pd.Series(data=True, index=sp_merge.index)
-    cond_diff = cond != cond_old
-
-    while np.sum(cond_diff) >= 1:
-        # .values is important otherwise the "=" would imply a join via the new index
-        sp_merge["next_id"] = sp_merge["id"].shift(-1).values
-
-        # identify rows to merge
-        cond1 = sp_merge["next_started_at"] - sp_merge["finished_at"] < datetime.timedelta(minutes=10)
-        cond2 = sp_merge["location_id"] == sp_merge["next_location_id"]
-        cond3 = sp_merge["location_id"] == sp_merge["next_location_id"]
-        cond = cond1 & cond2 & cond3
-
-        # assign index to next row
-        sp_merge.loc[cond, "id"] = sp_merge.loc[cond, "next_id"]
-        cond_diff = cond != cond_old
-        cond_old = cond.copy()
-
-        print(np.sum(cond_diff))
-
-    # aggregate values
-    sp_merged = sp_merge.groupby(by="id").agg(
-        {
-            "id": "first",
-            "index": "first",
-            "user_id": "first",
-            "trip_id": list,
-            "prev_trip_id": list,
-            "next_trip_id": list,
-            "started_at": "first",
-            "finished_at": "last",
-            "geom": "first",
-            "elevation": "first",
-            "location_id": "first",
-            "activity": "first",
-            "purpose": list
-            # "purpose_detected": list,
-            # "purpose_validated": list,
-            # "validated": "first",
-            # "validated_at": "first",
-        }
-    )
-
-    return sp_merged
+    return trips
 
 
-def filter_by_tracking_coverage(sp, tpls, coverage=0.99):
+def filter_user_by_number_of_days(sp, tpls, coverage=0.9, min_nb_good_days=30):
+    # could be replaced by https://github.com/mie-lab/trackintel/issues/258 once implemented
+    nb_users = len(sp.user_id.unique())
+
+    sp_tpls = sp.append(tpls)
+    coverage_df = ti.analysis.tracking_quality.temporal_tracking_quality(sp_tpls, granularity="day")
+
+    good_days_count = coverage_df[coverage_df["quality"] >= coverage].groupby(by="user_id")["quality"].count()
+    good_users = good_days_count[good_days_count >= min_nb_good_days].index
+    sp = sp[sp.user_id.isin(good_users)]
+    print("\t nb users now: ", len(sp.user_id.unique()), "before: ", nb_users)
+    return sp, good_users
+
+
+def filter_days_with_bad_tracking_coverage(sp, tpls, coverage=0.99):
 
     # could be replaced by https://github.com/mie-lab/trackintel/issues/258 once implemented
 
@@ -136,18 +101,18 @@ def filter_by_tracking_coverage(sp, tpls, coverage=0.99):
     sp_tpls_grouper = sp_tpls.groupby(["user_id", pd.Grouper(freq="D")])
     tracking_quality = sp_tpls_grouper["duration"].sum() / datetime.timedelta(days=1)
 
-    # np.sum(tracking_quality < 0.99)/tracking_quality.size
     # delete days with low tracking quality
     sp["started_at_day"] = pd.to_datetime(sp["started_at"].dt.date, utc=True)
     sp = sp.set_index(["user_id", "started_at_day"], drop=False)
     to_del_ix = tracking_quality[tracking_quality < coverage].index
-
+    nb_sp_old = sp.shape[0]
     sp.drop(sp.index.intersection(to_del_ix), axis=0, inplace=True)
     sp.set_index("id", drop=True, inplace=True)
+    print("\t nb dropped: ", nb_sp_old - sp.shape[0], "nb kept: ", sp.shape[0])
     return sp
 
 
-def generate_graphs(locs, sp, out_name, plotting=False):
+def generate_graphs(locs, sp, out_name, trips=None, plotting=False):
     AG_dict = {}
 
     for user_id_this in locs["user_id"].unique():
@@ -155,19 +120,24 @@ def generate_graphs(locs, sp, out_name, plotting=False):
         if sp_user.empty:
             continue
         locs_user = locs[locs["user_id"] == user_id_this]
-        AG = activity_graph(sp_user, locs_user)
+
+        if trips is not None:
+            trips_user = trips[trips["user_id"] == user_id_this]
+            AG = activity_graph(sp_user, locs_user, trips=trips_user)
+        else:
+            AG = activity_graph(sp_user, locs_user)
         if plotting:
             AG.plot(
                 os.path.join(".", "graph_images", "new", study, "spring", str(user_id_this)),
                 filter_node_importance=25,
                 draw_edge_label=False,
             )
-            AG.plot(
-                os.path.join(".", "graph_images", "new", study, "coords", str(user_id_this)),
-                filter_node_importance=25,
-                draw_edge_label=False,
-                layout="coordinate",
-            )
+            # AG.plot(
+            #     os.path.join(".", "graph_images", "new", study, "coords", str(user_id_this)),
+            #     filter_node_importance=25,
+            #     draw_edge_label=False,
+            #     layout="coordinate",
+            # )
         AG_dict[user_id_this] = copy.deepcopy(AG)
 
     # create graphs of the full period
@@ -175,7 +145,7 @@ def generate_graphs(locs, sp, out_name, plotting=False):
     pickle.dump(AG_dict, out_name)
 
 
-def generate_graphs_daily(locs, sp, out_name, plotting=False):
+def generate_graphs_daily(locs, sp, out_name, trips=None, plotting=False):
     # create daily graphs
     print("create index")
     sp.set_index("started_at", drop=False, inplace=True)
@@ -202,10 +172,10 @@ def generate_graphs_daily(locs, sp, out_name, plotting=False):
                 os.path.join(".", "graph_images", "new", "daily", user, "spring", day.strftime("%Y-%m-%d"),),
                 draw_edge_label=True,
             )
-            AG.plot(
-                os.path.join(".", "graph_images", "new", "daily", user, "coords", day.strftime("%Y-%m-%d"),),
-                layout="coordinate",
-            )
+            # AG.plot(
+            #     os.path.join(".", "graph_images", "new", "daily", user, "coords", day.strftime("%Y-%m-%d"),),
+            #     layout="coordinate",
+            # )
 
         AG_dict[user][day] = copy.deepcopy(AG)
         i = i + 1
@@ -218,7 +188,8 @@ def generate_graphs_daily(locs, sp, out_name, plotting=False):
 # globals
 # study name is used as schema name in database
 studies = ["yumuv_graph_rep"]  # , 'gc1']  # , 'geolife',]# 'tist_u1000', 'tist_b100', 'tist_b200', 'tist_u10000']
-limit = ""
+# studies = ['gc2', 'gc1']  # , 'geolife',]# 'tist_u1000', 'tist_b100', 'tist_b200', 'tist_u10000']
+limit = ""  # "where user_id < 4980"
 single_user = False
 
 if __name__ == "__main__":
@@ -239,18 +210,28 @@ if __name__ == "__main__":
         sp = get_staypoints(study=study, engine=engine)
         print("\t download triplegs")
         tpls = get_triplegs(study=study, engine=engine)
+
+        print("\t download trips")
+        trips = get_trips(study=study, engine=engine)
+
         print("\t download locations")
         locs = ti.io.read_locations_postgis(
             sql="select * from {}.locations".format(study), con=engine, geom_col="center",
         )
 
-        if single_user:
-            sp = sp[sp["user_id"] == "0d45bbc4-6c61-44a0-a7aa-d69311e3db40"]
+        # print("horizontal merge") # horizontal merge has to be done before trip generation in the import
 
-        sp_merged = horizontal_merge_staypoints(sp)
-        sp = filter_by_tracking_coverage(sp=sp_merged, tpls=tpls, coverage=0.99)
+        print("filter by tracking coverage")
+
+        sp, user_id_ix = filter_user_by_number_of_days(sp=sp, tpls=tpls)
+        tpls = tpls[tpls.user_id.isin(user_id_ix)]
+        trips = trips[trips.user_id.isin(user_id_ix)]
+        locs = locs[locs.user_id.isin(user_id_ix)]
 
         print("generate full graphs (transition counts)")
-        generate_graphs(locs=locs, sp=sp, out_name=open(GRAPH_OUTPUT + "_counts_full.pkl", "wb"))
+        generate_graphs(
+            locs=locs, sp=sp, out_name=open(GRAPH_OUTPUT + "_counts_full.pkl", "wb"), trips=trips, plotting=True
+        )
         print("generate daily graphs (transition counts)")
-        generate_graphs_daily(locs=locs, sp=sp, out_name=open(GRAPH_OUTPUT + "_daily_graphs.pkl", "wb"))
+        # sp = filter_days_with_bad_tracking_coverage(sp=sp_merged, tpls=tpls, coverage=0.99)
+        # generate_graphs_daily(locs=locs, sp=sp, out_name=open(GRAPH_OUTPUT + "_daily_graphs.pkl", "wb"))
