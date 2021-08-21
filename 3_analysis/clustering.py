97c0ab1a831991c258b19a64fdef5117f3675e7d
NinaWie <wnina@student.ethz.ch>
2021-08-21 18:48:34 +0200

    add other clustering methods

--- 3_analysis/clustering.py
+++ 3_analysis/clustering.py
@@ -1,13 +1,18 @@
 import numpy as np
 import pandas as pd
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
 
+algorithm_dict = {"kmeans": KMeans, "hierarchical": AgglomerativeClustering, "dbscan": DBSCAN}
 
-def normalize_and_cluster(feature_matrix, n_clusters=2, return_normed=False):
+def normalize_and_cluster(feature_matrix, algorithm="kmeans", n_clusters=2, return_normed=False):
+    """
+    Normalize feature matrix and cluster it 
+    algorithm: One of kmeans, hierarchical, dbscan
+    """
     std_cols = np.std(feature_matrix, axis=0)
     means_cols = np.mean(feature_matrix, axis=0)
     normed_feature_matrix = (feature_matrix - means_cols) / std_cols
-    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(normed_feature_matrix)
+    kmeans = algorithm_dict[algorithm](n_clusters=n_clusters).fit(normed_feature_matrix)
     if return_normed:
         return kmeans.labels_, normed_feature_matrix
     return kmeans.labels_
