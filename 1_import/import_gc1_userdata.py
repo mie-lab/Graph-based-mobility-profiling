9d84529974f170b958d4d30d60a2b9a7b426bfaa
Henry Martin <martinhe@ethz.ch>
2021-08-25 12:51:27 +0200

    ENH: Import user information for GC1

---
+++ 1_import/import_gc1_userdata.py
@@ -0,0 +1,33 @@
+import geopandas as gpd
+import pandas as pd
+import trackintel as ti
+from sqlalchemy import create_engine
+import numpy as np
+import os
+import json
+from future_trackintel.utils import horizontal_merge_staypoints
+
+CRS_WGS84 = "epsg:4326"
+#
+DBLOGIN_FILE = os.path.join(".", "dblogin.json")
+DBLOGIN_FILE_SOURCE = os.path.join(".", "dblogin_source.json")
+
+
+with open(DBLOGIN_FILE) as json_file:
+    LOGIN_DATA = json.load(json_file)
+
+with open(DBLOGIN_FILE_SOURCE) as json_file_source:
+    LOGIN_DATA_SOURCE = json.load(json_file_source)
+
+# build database login string from file
+conn_string = "postgresql://{user}:{password}@{host}:{port}/{database}".format(**LOGIN_DATA)
+conn_string_source = "postgresql://{user}:{password}@{host}:{port}/{database}".format(**LOGIN_DATA_SOURCE)
+
+userdata = pd.read_sql("select * from gc1.users", conn_string_source)
+columns_to_drop = ['workaddress', 'homeaddress', 'work_lon', 'work_lat',
+       'home_lon', 'home_lat', 'homebfs', 'workbfs', 'work_pc1', 'work_pc2',
+       'work_pc3', 'work_pc4', 'pc1', 'pc2', 'pc3', 'pc4', 'geom_home', 'geom_work']
+
+userdata.drop(columns_to_drop, axis=1, inplace=True)
+
+userdata.to_sql(name='user_info', con=conn_string, schema='gc1', index=False, if_exists='replace')
