fd1e61824c2983fc646c7f48def4ebbf27859ce7
Henry Martin <martinhe@ethz.ch>
2021-09-05 20:34:34 +0200

    BUG: add tracking quality filter

--- 2_preprocessing/generate_yumuv_before_after_graphs.py
+++ 2_preprocessing/generate_yumuv_before_after_graphs.py
@@ -11,7 +11,7 @@ from shapely.geometry import Point
 from sqlalchemy import create_engine
 import trackintel as ti
 from db_login import DSN  # database login information
-from generate_graphs import get_staypoints, get_triplegs, get_trips, get_locations, generate_graphs
+from generate_graphs import get_staypoints, get_triplegs, get_trips, get_locations, generate_graphs, filter_user_by_number_of_days
 from collections import defaultdict
 import pickle
 import numpy as np
@@ -21,12 +21,14 @@ engine = create_engine(
         **DSN
     )
 )
-
+file_prefix = "210905"
 study = "yumuv_graph_rep"
-sp_ = get_staypoints(study=study, engine=engine)
-tpls_ = get_triplegs(study=study, engine=engine)
-trips_ = get_trips(study=study, engine=engine)
-locs = get_locations(study=study, engine=engine)
+limit = "" #"where user_id in (5652, 5609, 4979, 5008, 6007)"
+sp_ = get_staypoints(study=study, engine=engine, limit=limit)
+tpls_ = get_triplegs(study=study, engine=engine, limit=limit)
+trips_ = get_trips(study=study, engine=engine, limit=limit)
+locs_ = get_locations(study=study, engine=engine, limit=limit)
+
 
 user_dates_kg = pd.read_sql("select * from henry_dev.user_dates where abo_start is not null", con=engine, index_col="user_id")
 
@@ -98,8 +100,8 @@ sp_after = sp_after_tg.append(sp_after_cg, ignore_index=True).set_index('id')
 tpls_after = tpls_after_tg.append(tpls_after_cg, ignore_index=True).set_index('id')
 trips_after = trips_after_tg.append(trips_after_cg, ignore_index=True).set_index('id')
 
-locs_before = locs.loc[sp_before['location_id'].unique()]
-locs_after = locs.loc[sp_after['location_id'].unique()]
+locs_before = locs_.loc[sp_before['location_id'].unique()]
+locs_after = locs_.loc[sp_after['location_id'].unique()]
 
 # postprocessing of gaps
 # because we filtered it can now be that a the origin of a trip is no longer in the set of staypoints. This would
@@ -114,6 +116,12 @@ destination_missing = ~trips_after['destination_staypoint_id'].isin(sp_after.ind
 trips_after.loc[origin_missing, 'origin_staypoint_id'] = pd.NA
 trips_after.loc[destination_missing, 'destination_staypoint_id'] = pd.NA
 
+# tracking quality
+print("\t\t drop users with bad coverage")
+sp_before, _ = filter_user_by_number_of_days(sp=sp_before, tpls=tpls_before, coverage=0.9, min_nb_good_days=14)
+sp_after, _ = filter_user_by_number_of_days(sp=sp_after, tpls=trips_after, coverage=0.9, min_nb_good_days=14)
+
+
 # only take intersection of users (users that are available in the before and after set)
 valid_users = np.intersect1d(sp_before.user_id.unique(), sp_after.user_id.unique(), assume_unique=True)
 
@@ -126,21 +134,24 @@ trips_after = trips_after[trips_after['user_id'].isin(valid_users)]
 locs_after = locs_after[locs_after['user_id'].isin(valid_users)]
 
 
+
+
 # generate graphs
 AG_dict_before = generate_graphs(locs=locs_before, sp=sp_before, study=study, trips=trips_before, plotting=False)
 AG_dict_after = generate_graphs(locs=locs_after, sp=sp_after, study=study, trips=trips_after, plotting=False)
 
+# generate output
 GRAPH_OUTPUT = os.path.join(".", "data_out", "graph_data", study)
 
-out_name_before = open(os.path.join(GRAPH_OUTPUT, "counts_full_before.pkl"), "wb")
-out_name_after = open(os.path.join(GRAPH_OUTPUT, "counts_full_after.pkl"), "wb")
+out_name_before = open(os.path.join(GRAPH_OUTPUT, "counts_full_before_{}.pkl".format(file_prefix)), "wb")
+out_name_after = open(os.path.join(GRAPH_OUTPUT, "counts_full_after_{}.pkl".format(file_prefix)), "wb")
 pickle.dump(AG_dict_before, out_name_before)
 pickle.dump(AG_dict_after, out_name_after)
 out_name_after.close()
 out_name_before.close()
 
 # test if reading works
-pkl_name_before = open(os.path.join(GRAPH_OUTPUT, "counts_full_before.pkl"), "rb")
-pkl_name_after = open(os.path.join(GRAPH_OUTPUT, "counts_full_after.pkl"), "rb")
+pkl_name_before = open(os.path.join(GRAPH_OUTPUT, "counts_full_before_{}.pkl".format(file_prefix)), "rb")
+pkl_name_after = open(os.path.join(GRAPH_OUTPUT, "counts_full_after_{}.pkl".format(file_prefix)), "rb")
 AG_dict_before2 = pickle.load(pkl_name_before)
 AG_dict_after2 = pickle.load(pkl_name_after)
