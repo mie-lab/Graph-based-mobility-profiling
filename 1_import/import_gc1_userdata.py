ac1d9179d23af5b047e255a4952470b1fafac076
Henry <martinhe@ethz.ch>
2022-03-08 14:27:38 +0100

    CLN: Update and check file

--- 1_import/import_gc1_userdata.py
+++ 1_import/import_gc1_userdata.py
@@ -1,11 +1,8 @@
-import geopandas as gpd
+"""Download user data from main database (commitdb) to the project database (graph_rep)"""
+
 import pandas as pd
-import trackintel as ti
-from sqlalchemy import create_engine
-import numpy as np
 import os
 import json
-from future_trackintel.utils import horizontal_merge_staypoints
 
 CRS_WGS84 = "epsg:4326"
 #
@@ -24,10 +21,27 @@ conn_string = "postgresql://{user}:{password}@{host}:{port}/{database}".format(*
 conn_string_source = "postgresql://{user}:{password}@{host}:{port}/{database}".format(**LOGIN_DATA_SOURCE)
 
 userdata = pd.read_sql("select * from gc1.users", conn_string_source)
-columns_to_drop = ['workaddress', 'homeaddress', 'work_lon', 'work_lat',
-       'home_lon', 'home_lat', 'homebfs', 'workbfs', 'work_pc1', 'work_pc2',
-       'work_pc3', 'work_pc4', 'pc1', 'pc2', 'pc3', 'pc4', 'geom_home', 'geom_work']
+columns_to_drop = [
+    "workaddress",
+    "homeaddress",
+    "work_lon",
+    "work_lat",
+    "home_lon",
+    "home_lat",
+    "homebfs",
+    "workbfs",
+    "work_pc1",
+    "work_pc2",
+    "work_pc3",
+    "work_pc4",
+    "pc1",
+    "pc2",
+    "pc3",
+    "pc4",
+    "geom_home",
+    "geom_work",
+]
 
 userdata.drop(columns_to_drop, axis=1, inplace=True)
 
-userdata.to_sql(name='user_info', con=conn_string, schema='gc1', index=False, if_exists='replace')
+userdata.to_sql(name="user_info", con=conn_string, schema="gc1", index=False, if_exists="replace")
